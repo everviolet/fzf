983b48c908f245ccf59a370cf8faedbc6c4ced2f
Sho Sakakibara <sho@sakakibara.dev>
2025-06-05 05:31:36 +0900

    feat: add generated themes

---
+++ themes/evergarden-fzf-fall.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#232A2E,bg:#232A2E,spinner:#AFDFE6,hl:#AFDFE6 \
+--color=fg:#F8F9E8,header:#AFDFE6,info:#96B4AA,pointer:#F7A182 \
+--color=marker:#F5D098,fg+:#F8F9E8,prompt:#F3C0E5,hl+:#AFDFE6 \
+--color=selected-bg:#3D494D \
+--color=border:#313B40,label:#F8F9E8"
